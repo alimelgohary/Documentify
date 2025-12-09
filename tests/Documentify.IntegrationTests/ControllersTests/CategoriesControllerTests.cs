165b4d10e8db9d413b2f1772461b223e54a7a242
alimelgohary <ali_algohary2001@outlook.com>
2025-12-09 03:50:19 +0200

    Add custom web app factory to use in memory db, implment 3 categories tests

--- tests/Documentify.IntegrationTests/ControllersTests/CategoriesControllerTests.cs
+++ tests/Documentify.IntegrationTests/ControllersTests/CategoriesControllerTests.cs
@@ -1,34 +1,96 @@
-﻿using Documentify.ApplicationCore.Features.Categories.GetAll;
-using Microsoft.AspNetCore.Mvc.Testing;
+﻿using Documentify.ApplicationCore.Features.Categories.Add;
+using Documentify.ApplicationCore.Features.Categories.GetAll;
+using Documentify.Infrastructure.Data;
+using Microsoft.Extensions.DependencyInjection;
 using System.Net;
 using System.Net.Http.Json;
 
 namespace Documentify.IntegrationTests.ControllersTests
 {
-    public class CategoriesControllerTests : IClassFixture<WebApplicationFactory<Program>>
+    public class CategoriesControllerTests : IClassFixture<CustomWebApplicationFactory<Program>>
     {
         private readonly HttpClient _client;
-
-        public CategoriesControllerTests(WebApplicationFactory<Program> factory)
+        private readonly CustomWebApplicationFactory<Program> _factory;
+        private readonly string path = "/categories";
+        public CategoriesControllerTests(CustomWebApplicationFactory<Program> factory)
         {
             _client = factory.CreateClient();
+            _factory = factory;
         }
 
         [Fact]
         public async Task Get_Categories_Returns_OK()
         {
-            var response = await _client.GetAsync("/categories");
+            string name = "New Category";
+            AddCategoryCommand newCategory = new(name);
+            await _client.PostAsJsonAsync(path, newCategory);
 
+            var response = await _client.GetAsync(path);
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
         }
 
         [Fact]
         public async Task Get_Categories_Returns_Items()
         {
-            var response = await _client.GetFromJsonAsync<GetAllCategoriesResponse>("/categories");
+            string name = "New Category for testing";
+            AddCategoryCommand newCategory = new(name);
+            await _client.PostAsJsonAsync(path, newCategory);
+            
+            var response = await _client.GetFromJsonAsync<GetAllCategoriesResponse>(path);
             Assert.NotNull(response);
             Assert.NotEmpty(response.items);
             Assert.Equal(response.count, response.items.Count);
+            Assert.Contains(response.items, c => c.Name == name);
+        }
+
+        [Fact]
+        public async Task Add_Category_Returns_Created()
+        {
+            string name = "New Category for testing";
+            AddCategoryCommand newCategory = new(name);
+            var response = await _client.PostAsJsonAsync(path, newCategory);
+            Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+
+            using var scope = _factory.Services.CreateScope();
+            var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+            Assert.True(db.Categories.Any(p => p.Name == name));
+        }
+
+        [Fact]
+        public async Task Add_Category_Returns_Guid()
+        {
+            string name = "New Category for testing 2";
+            AddCategoryCommand newCategory = new(name);
+
+            try
+            {
+                var response = await _client.PostAsJsonAsync(path, newCategory);
+                var createdResponse = await response.Content.ReadFromJsonAsync<AddCategoryResponse>();
+                
+                Assert.NotNull(createdResponse);
+                Assert.NotEqual(Guid.Empty, createdResponse!.categoryId);
+
+                using var scope = _factory.Services.CreateScope();
+                var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                
+                Assert.True(db.Categories.Any(p => p.Id == createdResponse.categoryId && p.Name == name));
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail($"Exception occurred while deserializing response: {ex.Message}");
+            }
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        [InlineData("ss")]
+        public async Task Add_Category_Invalid_Name_Returns_BadRequest(string? name)
+        {
+            AddCategoryCommand newCategory = new(name);
+            var response = await _client.PostAsJsonAsync(path, newCategory);
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
         }
     }
 
---
+++ tests/Documentify.IntegrationTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,26 @@
+﻿using Documentify.Infrastructure.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Documentify.IntegrationTests
+{
+    public class CustomWebApplicationFactory<T> : WebApplicationFactory<T> where T : class
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                var descriptor = services.Single(
+                    d => d.ServiceType == typeof(DbContextOptions<AppDbContext>));
+
+                services.Remove(descriptor);
+
+                services.AddDbContext<AppDbContext>(options =>
+                    options.UseInMemoryDatabase("TestDb"));
+            });
+        }
+    }
+
+}
