328a384720e06f918e98a2c3fd7aa3fc1532f3cf
alimelgohary <ali_algohary2001@outlook.com>
2025-12-25 19:17:49 +0200

    Exception handler returns a unified response

--- src/Documentify.Api/Middlewares/ExceptionHandlerMiddleware.cs
+++ src/Documentify.Api/Middlewares/ExceptionHandlerMiddleware.cs
@@ -1,4 +1,5 @@
 ﻿using Documentify.ApplicationCore.Common.Exceptions;
+using Documentify.ApplicationCore.Features;
 using FluentValidation;
 using Microsoft.AspNetCore.Mvc;
 
@@ -35,51 +36,40 @@ namespace Documentify.Api.Middlewares
         {
             var loggedErrors = ex.Errors.Select(e => new { e.PropertyName, e.ErrorMessage, e.AttemptedValue });
             _logger.LogInformation("Validation error: {@loggedErrors}", loggedErrors);
-            await Results.ValidationProblem(
-                errors: ex.Errors.GroupBy(x => x.PropertyName)
+            var errors = ex.Errors.GroupBy(x => x.PropertyName)
                                 .ToDictionary(
                                     g => g.Key,
                                     g => g.Select(e => e.ErrorMessage).ToArray()
-                                )
-            ).ExecuteAsync(context);
+                                );
+
+            await Results.BadRequest(Result.Failure("One or more validation errors occurred.", errors)).ExecuteAsync(context);
         }
         async Task HandleBadRequestExceptionAsync(BadRequestException ex, HttpContext context)
         {
             _logger.LogInformation("Bad Request: {Message}", ex.Message);
-            var problem = new ProblemDetails()
-            {
-                Status = StatusCodes.Status400BadRequest,
-                Detail = ex.Message
-            };
-            await Results.Problem(problem).ExecuteAsync(context);
+            
+            await Results.BadRequest(Result.Failure(ex.Message)).ExecuteAsync(context);
         }
         async Task HandleNotFoundExceptionAsync(NotFoundException ex, HttpContext context)
         {
             _logger.LogInformation("Not Found Exception: {Message}", ex.Message);
-            var problem = new ProblemDetails()
-            {
-                Status = StatusCodes.Status404NotFound,
-                Detail = ex.Message
-            };
-            await Results.Problem(problem).ExecuteAsync(context);
+            await Results.NotFound(Result.Failure(ex.Message)).ExecuteAsync(context);
         }
 
         async Task HandleUnknownExceptionAsync(Exception ex, HttpContext context)
         {
             _logger.LogError("Unhandled Exception {Message} {StackTrace}", ex.Message, ex.StackTrace);
-            var problem = new ProblemDetails()
-            {
-                Status = StatusCodes.Status500InternalServerError,
-                Title = "An internal server error occurred."
-            };
-
+            
+            Dictionary<string, string[]>? errors = default;
             if (_hostEnvironment.IsDevelopment())
             {
-                problem.Extensions.Add("message", ex.Message);
-                problem.Extensions.Add("stackTrace", ex.StackTrace);
+                errors = new();
+                errors.Add("Exception", [ex.Message, ex.StackTrace!]);
             }
 
-            await Results.Problem(problem).ExecuteAsync(context);
+            await Results.Json(data: Result.Failure("An internal server error occurred.", errors),
+                               statusCode: StatusCodes.Status500InternalServerError)
+                .ExecuteAsync(context);
         }
     }
 }
---
+++ src/Documentify.ApplicationCore/Features/Result.cs
@@ -0,0 +1,37 @@
+﻿namespace Documentify.ApplicationCore.Features
+{
+    public class Result
+    {
+        public bool IsSuccess { get; }
+        public string Message { get; }
+        public Dictionary<string, string[]>? Errors { get; }
+
+        protected Result(bool success, string message, Dictionary<string, string[]>? errors = null)
+        {
+            IsSuccess = success;
+            Message = message;
+            Errors = errors ?? new();
+        }
+
+        public static Result Success(string message = "")
+            => new(true, message);
+
+        public static Result Failure(string message, Dictionary<string, string[]>? errors = null)
+            => new(false, message, errors);
+    }
+    public class Result<T> : Result
+    {
+        public T? Value { get; }
+
+        private Result(bool success, T? value, string message, Dictionary<string, string[]>? errors)
+            : base(success, message, errors)
+            => Value = value;
+
+        public static Result<T> Success(T value, string message = "")
+            => new(true, value, message, null);
+
+        public static new Result<T> Failure(string message, Dictionary<string, string[]>? errors = null)
+            => new(false, default, message, errors);
+    }
+
+}
