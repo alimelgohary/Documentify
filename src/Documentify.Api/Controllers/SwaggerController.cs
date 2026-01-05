3f53875a237d68d37fa2e4930e28aa397986580c
alimelgohary <ali_algohary2001@outlook.com>
2026-01-05 15:39:17 +0200

    Add swagger controller to redirect users from root to swagger

---
+++ src/Documentify.Api/Controllers/SwaggerController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Documentify.Api.Controllers
+{
+    [ApiExplorerSettings(IgnoreApi = true)]
+    [ApiController]
+    public class SwaggerController : ControllerBase
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return Redirect("~/swagger/index.html");
+        }
+    }
+}
